3213c21170a7f1a2da32fbf19d64dd2c73ddf15e
stepin105068 <69070085+stepin105068@users.noreply.github.com>
2021-02-05 22:12:24 +0530

    ADDED: Daily wage of Fulltime Emp

--- empWageProblem.js
+++ empWageProblem.js
@@ -1,9 +1,30 @@
-console.log("Welcone to Employee Wage Problem");
-/* Checking Employee Status */
-let empCheck = Math.floor(Math.random() * 10) % 2;
+console.log("Welcome to Employee Wage Problem");
+//variable decleration
+
+const FULL_DAY_HOUR = 8;
+const WAGE_PER_HOUR = 20;
+let empCheck;
+let empCheck1;
+
+
+// Checking Employee Status
+
+empCheck = Math.floor(Math.random() * 10) % 2;
 if (empCheck == 1) {
     console.log("Employee Present");
 }
-if (empCheck == 0) {
+else if (empCheck == 0) {
     console.log("Employee Absent");
 }
+// function for calculating the daily emp wage
+
+function dailyWage()
+{
+   empCheck1 = Math.floor(Math.random() * 10) % 2;
+   if(empCheck1==1)
+   {
+        let dailyWage=FULL_DAY_HOUR*WAGE_PER_HOUR;
+        console.log("Daily wage of an employee is: "+dailyWage);
+   }
+}
+dailyWage();
