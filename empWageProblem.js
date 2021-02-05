dcc44dad77bfab450c880fb291140ab8b4cb17df
stepin105068 <69070085+stepin105068@users.noreply.github.com>
2021-02-05 22:45:39 +0530

    ADDED: Refactored using switch case

--- empWageProblem.js
+++ empWageProblem.js
@@ -5,36 +5,30 @@ const FULL_DAY_HOUR = 8;
 const PART_DAY_HOUR = 4;
 const WAGE_PER_HOUR = 20;
 const IS_PART_TIME = 0;
-const IS_FULL_TIME = 1;
+const IS_FULL_TIME=1 ;
+
+
 let finalWage;
 let empCheck;
-let empCheck1;
+
 
 
 
 // Checking Employee Status
 
-empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == 1) {
-    console.log("Employee Present");
+function getFinalWage() {
+    empCheck = Math.floor(Math.random() * 10) % 3;
+    switch (empCheck) {
+        case IS_FULL_TIME:
+            finalWage = FULL_DAY_HOUR * WAGE_PER_HOUR;
+            console.log("Daily wage of an full time employee is: "+finalWage);
+            break;
+        case IS_PART_TIME:
+            finalWage = PART_DAY_HOUR * WAGE_PER_HOUR;
+            console.log("Daily wage of a part time employee is: "+finalWage);
+            break;
+        default:
+            console.log("Employee is absent ");
+    }
 }
-else if (empCheck == 0) {
-    console.log("Employee Absent");
-}
-// function for calculating the daily emp wage
-
-function dailyWage()
-{
-   empCheck1 = Math.floor(Math.random() * 10) % 2;
-   if(empCheck1== IS_FULL_TIME)
-   {
-        finalWage=FULL_DAY_HOUR*WAGE_PER_HOUR;
-        console.log("Daily wage of an full time employee is: "+finalWage);
-   }
-   else if(empCheck1== IS_PART_TIME)
-   {
-        finalWage=PART_DAY_HOUR*WAGE_PER_HOUR;
-        console.log("Daily wage of a part time employee is: "+finalWage);
-   }
-}
-dailyWage();
+getFinalWage();
