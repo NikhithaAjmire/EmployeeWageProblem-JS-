b2aff1929d80770fe668eb9c7dd20da676718765
stepin105068 <69070085+stepin105068@users.noreply.github.com>
2021-02-05 21:45:06 +0530

    ADDED: Welcome Message

---
+++ empWageProblem.js
@@ -0,0 +1,10 @@
+console.log("Welcone to Employee Wage Problem");
+
+/* Checking for Employee status */
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == 1) {
+    console.log("Employee Present.");
+}
+if (empCheck == 0) {
+    console.log("Employee  Absent");
+}
