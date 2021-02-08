d240cb5ad941e2a71a4327bac670a01b3ed2ab77
NikhithaAjmire <77086546+NikhithaAjmire@users.noreply.github.com>
2021-02-08 20:10:26 +0530

    Update empWageProblem.js

--- empWageProblem.js
+++ empWageProblem.js
@@ -45,33 +45,3 @@ console.log("Total wage of month is : "+finalWage);
 
 
 
-/*const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const IS_ABSENT = 0;
-const FULL_TIME_HOURS = 8;
-const PART_TIME_HOURS = 4;
-const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 20;
-let workingDays = 1;
-let totalWorkingHours = 0;
-function getWorkingHours() {
-    empCheck = Math.floor(Math.random() * 10) % 3;
-    switch (empCheck) {
-        case IS_PART_TIME:
-            return PART_TIME_HOURS;
-            break;
-        case IS_FULL_TIME:
-            return FULL_TIME_HOURS;
-            break;
-        case IS_ABSENT:
-            return IS_ABSENT;
-            break;
-    }
-}
-while (workingDays <= 20) {
-    workingDays++;
-    let empHrs = getWorkingHours();
-    totalWorkingHours += empHrs;
-}
-totalWage = totalWorkingHours * WAGE_PER_HOUR;
-console.log("Total wage of month is : "+totalWage);*/
