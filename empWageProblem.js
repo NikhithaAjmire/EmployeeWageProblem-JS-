dd1b708351e82e2ed3f24a528b58438ca16fa7bf
stepin105068 <69070085+stepin105068@users.noreply.github.com>
2021-02-08 20:17:11 +0530

    ADDED: Calculated until reach max hours

--- empWageProblem.js
+++ empWageProblem.js
@@ -8,6 +8,8 @@ const IS_PART_TIME = 0;
 const IS_FULL_TIME=1 ;
 const IS_ABSENT = 2;
 const NUM_OF_WORK_DAYS = 20;
+
+const MAX_HOURS_IN_MONTH = 100;
 let workDays = 1;
 let totalWorkHours = 0;
 
@@ -33,7 +35,7 @@ function getFinalWage() {
             break;
     }
 }
-while (workDays <= 20) {
+while (workDays <= NUM_OF_WORK_DAYS && totalWorkHours <= MAX_HOURS_IN_MONTH) {
     workDays++;
     let empHrs = getFinalWage();
     totalWorkHours += empHrs;
@@ -45,33 +47,3 @@ console.log("Total wage of month is : "+finalWage);
 
 
 
-/*const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const IS_ABSENT = 0;
-const FULL_TIME_HOURS = 8;
-const PART_TIME_HOURS = 4;
-const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 20;
-let workingDays = 1;
-let totalWorkingHours = 0;
-function getWorkingHours() {
-    empCheck = Math.floor(Math.random() * 10) % 3;
-    switch (empCheck) {
-        case IS_PART_TIME:
-            return PART_TIME_HOURS;
-            break;
-        case IS_FULL_TIME:
-            return FULL_TIME_HOURS;
-            break;
-        case IS_ABSENT:
-            return IS_ABSENT;
-            break;
-    }
-}
-while (workingDays <= 20) {
-    workingDays++;
-    let empHrs = getWorkingHours();
-    totalWorkingHours += empHrs;
-}
-totalWage = totalWorkingHours * WAGE_PER_HOUR;
-console.log("Total wage of month is : "+totalWage);*/
